5698c6ab9b8f0f46e916767aa4a4182dee6b614c
mmangold7 <mmangold7@gmail.com>
2018-05-06 13:58:02 -0400

    added ef and blog model, changed gitignore a bit

--- Startup.cs
+++ Startup.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using mmangold.com.Models;
 
 namespace mmangold.com
 {
@@ -22,6 +24,9 @@ namespace mmangold.com
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            var connection = @"Server=(localdb)\mssqllocaldb;Database=EFGetStarted.AspNetCore.NewDb;Trusted_Connection=True;ConnectRetryCount=0";
+            services.AddDbContext<BloggingContext>(options => options.UseSqlServer(connection));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
